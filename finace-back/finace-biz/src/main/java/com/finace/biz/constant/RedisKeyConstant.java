0b9aabbb8441847eb35cd286d9f0387a18372338
Mingkai Liu <1793645613l@gmail.com>
2024-03-07 02:43:20 +0800

    feat: 完成验证码保存到redis中

---
+++ finace-back/finace-biz/src/main/java/com/finace/biz/constant/RedisKeyConstant.java
@@ -0,0 +1,25 @@
+package com.finace.biz.constant;
+
+public class RedisKeyConstant {
+    // 图形验证码 前缀
+    public static final String GRAPHIC_VERIFICATION_CODE = "GRAPHIC_VERIFICATION_CODE:";
+
+    /**
+     * 短信验证码
+     */
+    public static final String SMS_CODE = "SMS_CODE:";
+
+    public static final String REG_CODE_KEY = "REG_CODE:";
+
+    public static final String CLIENT_TOKEN_KEY = "CLIENT_TOKEN_KEY:";
+
+    /**
+     * 修改菜单锁
+     */
+    public static final String CHANGE_MENU_LOCK = "CHANGE_MENU_LOCK";
+
+    /**
+     * 修改资源锁
+     */
+    public static final String CHANGE_RESOURCE_LOCK = "CHANGE_RESOURCE_LOCK";
+}
--- finace-back/finace-biz/src/main/java/com/finace/biz/service/impl/MemberLoginServiceImpl.java
+++ finace-back/finace-biz/src/main/java/com/finace/biz/service/impl/MemberLoginServiceImpl.java
@@ -5,13 +5,22 @@ import cn.hutool.captcha.LineCaptcha;
 import com.finace.biz.dto.form.GetBase64CodeForm;
 import com.finace.biz.service.MemberLoginService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import static com.finace.biz.constant.RedisKeyConstant.GRAPHIC_VERIFICATION_CODE;
 
 @Service
 @Slf4j
 public class MemberLoginServiceImpl implements MemberLoginService {
+
+    @Autowired
+    RedisTemplate<String, Object> redisTemplate;
+
     @Override
     public String getClinetId() {
         return UUID.randomUUID().toString().replace("-", "");
@@ -21,7 +30,8 @@ public class MemberLoginServiceImpl implements MemberLoginService {
     public String getBase64Code(GetBase64CodeForm form) {
         LineCaptcha lineCaptcha = CaptchaUtil.createLineCaptcha(300, 192, 5, 150);
         String code = lineCaptcha.getCode();
-        // TODO 保存验证码到redis中
+        // 保存验证码到redis中, 15分钟过期
+        redisTemplate.opsForValue().set(GRAPHIC_VERIFICATION_CODE + form.getClientId(), code, 15, TimeUnit.MINUTES);
         return lineCaptcha.getImageBase64();
     }
 }
